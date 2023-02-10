daf1d7dd8049db6b17cf598cd38de5d24e1511c6
Elia Kuzmanova <eliamitova7@gmail.com>
2023-02-10 11:10:44 +0100

    add login and register page

---
+++ skeleton/controllers/authController.js
@@ -0,0 +1,15 @@
+exports.getRegisterView = (req, res) => {
+    res.render("auth/register")
+}
+
+// exports.postRegister = (req, res) => {
+
+// }
+
+exports.getLoginView = (req, res) => {
+    res.render("auth/login")
+}
+
+// exports.postLogin = (req, res) => {
+
+// }
