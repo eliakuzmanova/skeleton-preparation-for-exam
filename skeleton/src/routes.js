eae74afbeb1161636032e5f96b0a14ba4e401316
Elia Kuzmanova <eliamitova7@gmail.com>
2023-02-11 13:45:08 +0100

    add dynamic navigation

--- skeleton/src/routes.js
+++ skeleton/src/routes.js
@@ -2,6 +2,9 @@ const router = require("express").Router();
 
 const homeController = require("../controllers/homeController")
 const authController = require("../controllers/authController")
+const {authMiddleware} = require("../middlewares/authMiddleware")
+
+//add middleware to the needed routes <<<<-----------------------
 
 router.get("/", homeController.getHomeView)
 router.get("/404", homeController.get404View)
